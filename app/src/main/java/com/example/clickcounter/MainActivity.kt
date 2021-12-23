4f38465ba5a39dff3bc3f3e52dc44395639ff094
ziddi <azam13bh@gmail.com>
2021-12-23 14:44:05 +0600

    first commit 0.2

--- app/src/main/java/com/example/clickcounter/MainActivity.kt
+++ app/src/main/java/com/example/clickcounter/MainActivity.kt
@@ -15,17 +15,26 @@ class MainActivity : AppCompatActivity() {
         // get reference to button
         val btnClickMe = findViewById(R.id.button) as Button
         val myTextView = findViewById(R.id.textView) as TextView
+        val mytextView1 = findViewById(R.id.textView1) as TextView
         var counter = 0
+        var store = 0
+
         // set on-click listener
         btnClickMe.setOnClickListener {
             counter = counter + 1
-            myTextView.text = counter.toString()
-            Toast.makeText(this@MainActivity, "clicked", Toast.LENGTH_SHORT).show()
+            store = counter
+            myTextView.text = "You Clicked : " + counter.toString()
         }
         myTextView.setOnClickListener{
+            mytextView1.text = "Save :" + store
             counter = 0
-            myTextView.text = counter.toString()
+            myTextView.text = "You Clicked : " + counter.toString()
             Toast.makeText(this@MainActivity, "restored", Toast.LENGTH_SHORT).show()
         }
+        mytextView1.setOnClickListener{
+            counter = store
+            myTextView.text = "You Clicked : " + counter.toString()
+        }
+
     }
 }
